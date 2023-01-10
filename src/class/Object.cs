4a8a12ec77a4cf8cb0bbc1d34e400b2265eb1ec1
NLAKHDARI <nagib.lakhdari@ynov.com>
2023-01-10 16:41:12 +0100

    object

--- src/class/Object.cs
+++ src/class/Object.cs
@@ -1,4 +1,20 @@
 namespace src.Object;
 class Object{
+    private int _position;
+    public int positionObj{
+        get => _position;
+        set => _position = value;
+    }
+    public string positionObjStr{
+        get => _position.ToString();
+    }
 
+    private int _weight;
+    public int weightObj{
+        get => _weight;
+        set => _weight = value;
+    }
+    public string weightObjStr{
+        get => _weight.ToString();
+    }
 }
