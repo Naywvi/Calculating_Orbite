e376293454e60074c8bc6f0f96d80d473f395455
NLAKHDARI <nagib.lakhdari@ynov.com>
2023-01-10 16:35:32 +0100

    position

--- Program.cs
+++ Program.cs
@@ -1,10 +1,12 @@
-﻿namespace main
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            
+﻿using src.Position;
+
+namespace main{
+    class Program{
+        static void Main(string[] args){
+            var p = new Postion();
+            p.x = 10;
+            p.y = 30;
+            Console.WriteLine(p.myPosition());
         }
     }
 }
--- src/class/Position.cs
+++ src/class/Position.cs
@@ -1,4 +1,23 @@
 namespace src.Position;
 class Postion{
-
+    private int _x;
+    public int x{
+        get => _x;
+        set => _x = value;
+    }
+    public string xString{
+        get => _x.ToString();
+    }
+    private int _y;
+    public int y{
+        get => _y;
+        set => _y = value;
+    }
+    public string yString{
+        get => _y.ToString();
+    }
+    public string myPosition(){
+        var postionString = "\n position:  x: "+this.yString+" km \n            y: "+this.xString+" km\n";
+        return postionString;
+    }
 }
