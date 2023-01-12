3b791f4840ee6b06bed4b60604a6d32aaad3e9d3
Naywvi <nagib.lakhdari.pro@gmail.com>
2023-01-12 20:43:33 +0100

    update

--- Program.cs
+++ Program.cs
@@ -7,21 +7,20 @@ namespace main{
         static void Main(string[] args){
             var main = new Program();
 
-            Console.WriteLine("Welcome to the Orbit project !");
+            Console.WriteLine(main._sentences[5]);
 
             var pos = new Position();
             pos.constructorPosition(main.generatePostion(1),main.generatePostion(2));
 
-            var obj = new Obj();
-            //obj.diameter = main.generatePostion(4);
-            obj.constructorObject(obj.weightObj,obj.position);
-
-            var planet = new PlanetClass();
-            planet.constructorPlanet(main.generatePostion(7), main.generatePostion(5), main.generatePostion(6));
+            var planet = new PlanetClass();//Object start pos = 0 need launch
+            planet.constructorPlanet(main.generatePostion(3), 0, main.generatePostion(4));
+        
+            //Console.WriteLine(pos.showPosition());
+            //Console.WriteLine(planet.diameter + " - " + planet.obj.position + " = " + planet.obj.weightObj);
         }
 
         private int generatePostion(int sentence){
-            Console.WriteLine("Please enter",_sentences[sentence]);
+            Console.WriteLine("Please enter" + _sentences[sentence]);
             var _OstdIn = Console.ReadLine();
             if (!int.TryParse(_OstdIn, out int result)){
                 Console.WriteLine(_sentences[0]);
@@ -30,16 +29,12 @@ namespace main{
             return result;
         }
         private string[] _sentences = new string[] {
-            "Entrée non valide. Veuillez entrer un nombre valide.",
+            "Bad input try again.",
             " a starting x position for your object (in km) : ",
             " a starting y position for your object (in km) : ",
-            " a starting position for your object :",
-            " a starting position for your object : ",
-            " the mass of your object : ",
-            " the diameter of your object : ",
-            " a starting position for your planet : ",
-            " the mass of your planet : ",
-            " the diameter of your planet : "
+            " the weight of your object : ",
+            " the diameter of your planet : ",
+            "Welcome to the Orbit project !",
             };
     }
 }
