9e739573efb342747b7cc8ec3e4777450ff04ce2
LMARTIN6 <luc.martin@ynov.com>
2023-01-13 23:04:21 +0100

    passage des int en double

--- Program.cs
+++ Program.cs
@@ -4,10 +4,12 @@ using src.Simulation;
 
 namespace main
 {
-    class Program{
+    class Program
+    {
 
         //Setup all constructors
-        static void Main(string[] args){
+        static void Main(string[] args)
+        {
             var main = new Program();
             Console.WriteLine(main._sentences[7]);
 
@@ -18,34 +20,41 @@ namespace main
             planet.constructorPlanet(main.userPromp(3), 0, main.userPromp(4));
 
             var simulation = new Simulation();
-            simulation.constructorSimulation(planet,pos,main.userPromp(5),main.userPromp(6));
+            simulation.constructorSimulation(planet, pos, main.userPromp(5), main.userPromp(6));
         }
 
         //Generate from user input for constructors
-        private int userPromp(int sentence){
+        private int userPromp(int sentence)
+        {
             Console.WriteLine("[+] - Please enter" + _sentences[sentence]);
             var _OstdIn = Console.ReadLine();
-            if (sentence == 6){//Special verification for degree
-                
-                if(int.Parse(_OstdIn) > 180 || int.Parse(_OstdIn) <= 0){//Check if input (>180 & <= 0)
+            if (sentence == 6)
+            {//Special verification for degree
+
+                if (int.Parse(_OstdIn) > 180 || int.Parse(_OstdIn) <= 0)
+                {//Check if input (>180 & <= 0)
                     Console.WriteLine(_sentences[0]);
                     return userPromp(6);
                 }
-                if (!int.TryParse(_OstdIn, out int result) ){
+                if (!int.TryParse(_OstdIn, out int result))
+                {
                     Console.WriteLine(_sentences[0]);
                     _OstdIn = Console.ReadLine();
                 }
                 return result;
 
-            }else{
-                if (!int.TryParse(_OstdIn, out int result)){ // check if the input is an integer
+            }
+            else
+            {
+                if (!int.TryParse(_OstdIn, out int result))
+                { // check if the input is an integer
                     Console.WriteLine(_sentences[0]);
                     _OstdIn = Console.ReadLine();
                 }
                 return result;
             }
         }
-        
+
         private string[] _sentences = new string[] {
             "[-] - Bad input try again.",
             " a starting x position for your object (km) : ",
