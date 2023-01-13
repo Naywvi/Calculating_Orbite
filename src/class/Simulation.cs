1d53f507838ad8349e9a8ca635090ce18d6c8160
LMARTIN6 <luc.martin@ynov.com>
2023-01-13 21:28:37 +0100

    finission avec tous les calculs

--- src/class/Simulation.cs
+++ src/class/Simulation.cs
@@ -2,28 +2,32 @@ using src.Planet;
 using src.Object;
 using src.Position;
 
-namespace src.Simulation{
+namespace src.Simulation
+{
 
-        public class Simulation : PlanetClass {
+    public class Simulation : PlanetClass
+    {
         private int _speed;
-        public int speed{
+        public int speed
+        {
             get => _speed;
             set => _speed = value;
         }
         private int _throwingAngle;
-        public int throwingAngle{
+        public int throwingAngle
+        {
             get => _throwingAngle;
             set => _throwingAngle = value;
         }
 
         //Constructor simulation
-        public void constructorSimulation(PlanetClass planet,Pos pos,int speedP,int throwingAngleP){
+        public void constructorSimulation(PlanetClass planet, Pos pos, int speedP, int throwingAngleP)
+        {
             speed = speedP;
             throwingAngle = throwingAngleP;
 
             Console.WriteLine(pos.showPosition());
             Console.WriteLine(planet.diameter + " - " + planet.obj.position + " = " + planet.obj.weightObj);
-        
         }
     }
 }
