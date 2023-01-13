4722f3a5b67263b9e99f7033b93133223982bbdd
LMARTIN6 <luc.martin@ynov.com>
2023-01-14 00:38:55 +0100

    pour que ton pc crame pas

--- src/class/Simulation.cs
+++ src/class/Simulation.cs
@@ -30,13 +30,13 @@ namespace src.Simulation
             Console.WriteLine(pos.showPosition());
             Console.WriteLine(planet.diameter + " - " + planet.obj.position + " = " + planet.obj.weightObj);
             Console.WriteLine("Simulation started with speed = " + speed + " and throwing angle = " + throwingAngle);
-            while (pos.y != 0)
-            {
-                double distance = constMethods.generateDistance(pos.x, pos.y, planet.diameter, planet.obj.position, planet.obj.position);
-                (double vectorX, double vectorY) = constMethods.GenerateVectorDirection(throwingAngle, speed);
-                double gravityStrength = constMethods.generateGravityStrength(planet.obj.weightObj, Math.Pow(5.972 * 10, 24), distance); // Math.pow(etc) = mass type of a planet (here earth)
+            // while (pos.y != 0)  
+            // {
+            //     double distance = constMethods.generateDistance(pos.x, pos.y, planet.diameter, planet.obj.position, planet.obj.position);
+            //     (double vectorX, double vectorY) = constMethods.GenerateVectorDirection(throwingAngle, speed);
+            //     double gravityStrength = constMethods.generateGravityStrength(planet.obj.weightObj, Math.Pow(5.972 * 10, 24), distance); // Math.pow(etc) = mass type of a planet (here earth)
 
-            }
+            // }
         }
     }
 }
